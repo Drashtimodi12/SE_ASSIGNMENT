aee9dd873ce84a15a48cf6b8e2a524fa5e23ca4c
Drashti33 <drashtimodi33@gmail.com>
2024-12-18 09:07:05 +0530

    SE1

--- Assignment/C++Program/Assignment_3/Variables_DataTypes_&_Operators/Q6.cpp
+++ Assignment/C++Program/Assignment_3/Variables_DataTypes_&_Operators/Q6.cpp
@@ -1,4 +1,29 @@
-/*6.	Type Conversion:- 
+/*6.	Type Conversion:-
 Write a C++ program that performs both implicit and explicit type conversions and prints the results.
 •	Objective: Practice type casting in C++.
-*/
+
+// Implicit
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 10;
+    char y = 'a';
+    // y implicitly converted to int. ASCII
+    x = x + y;
+    // x is implicitly converted to float
+    float z = x + 1.0;
+    cout << "x = " << x << endl
+         << "y = " << y << endl
+         << "z = " << z << endl;
+}*/
+// Explicict
+#include <iostream>
+using namespace std;
+int main()
+{
+    double x = 1.2;
+    // Explicit conversion from double to int
+    int sum = (int)x + 1;
+    cout << "Sum = " << sum;
+}
